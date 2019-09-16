5eb8e588b9f72975d5051be7003289ac9bfeef51
vy3191 <venkyafhs@gmail.com>
2019-09-15 20:42:25 -0400

    Added Navbar page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-
+import WelcomePage from './components/WelcomePage';
+import {Route} from 'react-router-dom';
+import axios from 'axios';
 
 export default function App() {
+  const [pages, setPages] = useState('');
+
+  useEffect(() => {
+     axios.get(`https://rick-api.herokuapp.com/api/`)
+          .then( response => {
+              console.log(response)
+          })
+          .catch(error=> {
+             console.log(error);
+          })
+  },[])
+
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
+      <TabNav />
+      <WelcomePage />
+     
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
@@ -7,9 +7,40 @@ import { NavLink } from "react-router-dom";
 // Review Semantic UI Component options for nav-like UI:
 // https://react.semantic-ui.com/collections/menu/
 // https://react.semantic-ui.com/modules/tab/
-// https://react.semantic-ui.com/elements/button/
-// https://react.semantic-ui.com/collections/breadcrumb/
+// https://react.semantic-ui.com/elements/button/rumb/
+// https://react.semantic-ui.com/collections/breadc
 
-export default function TabNav() {
+export default class TabNav extends React.Component {
+  state = { activeItem: 'Home Page' }
 
-};
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu tabular>
+        <Menu.Item
+          name='Home Page'
+          active={activeItem === 'Home Page'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Characters'
+          active={activeItem === 'Characters'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Locations'
+          active={activeItem === 'Locations'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Episodes'
+          active={activeItem === 'Episodes'}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
+    )
+  }
+}
