9b6032318361950de85aa030190c199eb58d5ff8
vy3191 <venkyafhs@gmail.com>
2019-09-15 20:46:10 -0400

    Added NavLinks to the tabs

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,14 +2,6 @@ import React, {useState, useEffect} from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-// TODO: Add missing menu/tabs/nav below
-
-// Review Semantic UI Component options for nav-like UI:
-// https://react.semantic-ui.com/collections/menu/
-// https://react.semantic-ui.com/modules/tab/
-// https://react.semantic-ui.com/elements/button/rumb/
-// https://react.semantic-ui.com/collections/breadc
-
 export default class TabNav extends React.Component {
   state = { activeItem: 'Home Page' }
 
@@ -20,26 +12,34 @@ export default class TabNav extends React.Component {
 
     return (
       <Menu tabular>
-        <Menu.Item
-          name='Home Page'
-          active={activeItem === 'Home Page'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Characters'
-          active={activeItem === 'Characters'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Locations'
-          active={activeItem === 'Locations'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Episodes'
-          active={activeItem === 'Episodes'}
-          onClick={this.handleItemClick}
-        />
+        <NavLink>
+            <Menu.Item
+              name='Home Page'
+              active={activeItem === 'Home Page'}
+              onClick={this.handleItemClick}
+            />
+        </NavLink>
+        <NavLink>
+            <Menu.Item
+              name='Characters'
+              active={activeItem === 'Characters'}
+              onClick={this.handleItemClick}
+            />
+        </NavLink>
+        <NavLink>
+            <Menu.Item
+              name='Locations'
+              active={activeItem === 'Locations'}
+              onClick={this.handleItemClick}
+            />
+        </NavLink>
+        <NavLink>
+            <Menu.Item
+              name='Episodes'
+              active={activeItem === 'Episodes'}
+              onClick={this.handleItemClick}
+            />
+        </NavLink>
       </Menu>
     )
   }
